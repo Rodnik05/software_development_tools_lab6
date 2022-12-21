f82e0ff6a4e0905427f768171b8f05248aaf5fa6
Nikita <nrodetckii@mail.ru>
2022-12-21 08:22:13 +0300

    first commit

---
+++ calc.py
@@ -0,0 +1,35 @@
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def div(a, b):
+    return a / b
+
+
+def mul(a, b):
+    return a * b 
+
+
+def calc(a, b, op):
+    if op == '+':
+        return add(a, b)
+    elif op == '-':
+        return sub(a, b)
+    elif op == '/':
+        return div(a, b)
+    elif op == '*':
+        return mul(a, b)
+    else:
+        return "Error"
+
+
+if __name__ == '__main__':
+    a, op, b = input().split()
+    a = int(a)
+    op = str(op)
+    b = int(b)
+    print(calc(a, b, op))
---
+++ tests/test_pytest.py
@@ -0,0 +1,10 @@
+import calc
+
+
+def test_add():
+    assert calc.add(3, 5) == 8
+
+
+def test_sub():
+    assert calc.sub(3, 5) == -2
+    
